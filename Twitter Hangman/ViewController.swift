26fce7c9e92da55311cef13e0e828a67fda67399
Jamone Kelly <xg.monie089@gmail.com>
2016-01-12 02:47:20 -0500

    Initial Commit

---
+++ Twitter Hangman/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twitter Hangman
+//
+//  Created by Jamone Alexander Kelly on 1/12/16.
+//  Copyright © 2016 Jamone Kelly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Twitter HangmanTests/Twitter_HangmanTests.swift
@@ -0,0 +1,36 @@
+//
+//  Twitter_HangmanTests.swift
+//  Twitter HangmanTests
+//
+//  Created by Jamone Alexander Kelly on 1/12/16.
+//  Copyright © 2016 Jamone Kelly. All rights reserved.
+//
+
+import XCTest
+@testable import Twitter_Hangman
+
+class Twitter_HangmanTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
